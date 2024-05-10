368e33c91231e036c16cd1d926d33021380a2b24
jonas-soderholm <jonas.soderholm89@gmail.com>
2024-05-10 17:05:35 +0200

    Update env files

--- server/folketsserver/folkets_app/mongo_connection.py
+++ server/folketsserver/folkets_app/mongo_connection.py
@@ -5,25 +5,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mongodb_uri = os.getenv('MONGODB_URI')
+mongodb_name = os.getenv('MONGO_DB_NAME')
+mongodb_collection = os.getenv('MONGO_DB_COLLECTION')
 
 client = pymongo.MongoClient(mongodb_uri)
 
 #Define DB Name
-dbname = client['folketsdb']
+dbname = client[mongodb_name]
 
 #Define Collection
-collection = dbname['commercials']
+collection = dbname[mongodb_collection]
 
-commercial={
-    "name": "Sammy",
-    "current_donation_amount" : 2000,
-    "max_donation_amount" : 5000,
-    "info" : "Shark",
-    "amount_of_donations" : 48,
-    "status" : "active",
-    "img" : "email@example.com"
-}
+# commercial={
+#     "name": "Sammy",
+#     "current_donation_amount" : 2000,
+#     "max_donation_amount" : 5000,
+#     "info" : "Shark",
+#     "amount_of_donations" : 48,
+#     "status" : "active",
+#     "img" : "email@example.com"
+# }
 
-collection.insert_one(commercial)
+# collection.insert_one(commercial)
 
-user_details = collection.find({})
+# user_details = collection.find({})
+
+
