8f84b06b6f1ce32f9ba2df8e63f7ed719d742aec
jonas-soderholm <jonas.soderholm89@gmail.com>
2024-05-14 17:03:43 +0200

    Update envs

--- client/src/components/pages/DonationDetail.jsx
+++ client/src/components/pages/DonationDetail.jsx
@@ -29,34 +29,40 @@ function DonationDetail() {
   }, [id]);
 
   return (
-    <div className="mb-[70vh] mx-4">
+    <div className="mx-4">
       {filteredCommercial ? (
-        <div className="flex flex-row flex-wrap items-center md:my-[15rem] justify-center mt-[2rem] ">
+        <div className="flex flex-row flex-wrap items-center justify-center py-[3rem]">
           <div className="image">
             <img
-              className="md:w-[25rem] md:h-auto h-auto w-[20rem] rounded-lg object-cover mb-[2rem]"
+              className="md:w-[25rem] md:h-auto h-auto w-[20rem] rounded-lg object-cover md:mb-0 mb-3 "
               src={filteredCommercial.img}
               alt=""
             />
           </div>
           <div className="information flex md:flex-row md:items-start md:text-left">
             {/* <div className="information mx-auto my-10 flex max-w-xs flex-col items-center rounded-xl border-4 px-4 py-4 text-center md:max-w-lg md:flex-row md:items-start md:text-left"> */}
-            <div className="mb-4 md:mr-6 md:mb-0"></div>
+            <div className="mb-4 md:mr-6"></div>
             <div className="">
               <p className="text-xl font-medium text-gray-700">{filteredCommercial.name}</p>
               <p className="mb-4 text-sm font-medium text-gray-500">{filteredCommercial.status}</p>
               <div className="flex justify-between ">
                 <div className="flex flex-col items-center rounded-xl bg-gray-100 md:px-5 px-4 py-2">
                   <p className="text-sm font-medium text-gray-500">Donationer</p>
-                  <p className="text-3xl font-medium text-gray-600">{filteredCommercial.amount_of_donations}</p>
+                  <p className="md:text-3xl text-2xl font-medium text-gray-600">
+                    {filteredCommercial.amount_of_donations}
+                  </p>
                 </div>
                 <div className="flex flex-col items-center rounded-xl bg-gray-100 md:px-5 px-4 py-2">
                   <p className="text-sm font-medium text-gray-500">Totalt</p>
-                  <p className="text-3xl font-medium text-gray-600">{filteredCommercial.current_donation_amount}kr</p>
+                  <p className="md:text-3xl text-2xl font-medium text-gray-600">
+                    {filteredCommercial.current_donation_amount}kr
+                  </p>
                 </div>
                 <div className="flex flex-col items-center rounded-xl bg-gray-100 md:px-5 px-4 py-2">
                   <p className="text-sm font-medium text-gray-500">Mål</p>
-                  <p className="text-3xl font-medium text-gray-600">{filteredCommercial.max_donation_amount}kr</p>
+                  <p className="md:text-3xl text-2xl font-medium text-gray-600">
+                    {filteredCommercial.max_donation_amount}kr
+                  </p>
                 </div>
               </div>
               <div className="my-6  max-w-[24rem]">
